0d28f8b6701807fb80379965e3fc33136f437f88
Dustin <clerin+gitkraken@gmail.com>
2019-11-15 09:22:24 -0800

    stretch for names.py finished

--- names/names.py
+++ names/names.py
@@ -14,10 +14,28 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"original runtime: {end_time - start_time} seconds")
+
+# REFACTORED
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []
 
 bst = None
 setup = False
@@ -35,5 +53,34 @@ for name_2 in names_2:
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"refactored runtime: {end_time - start_time} seconds")
 
+# STRETCH
+
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []
+compare = []
+
+bst = None
+setup = False
+for name_1 in names_1:
+    compare.append(name_1)
+
+for name_2 in names_2:
+    if name_2 in compare:
+        duplicates.append(name_2)
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"stretch runtime: {end_time - start_time} seconds")
